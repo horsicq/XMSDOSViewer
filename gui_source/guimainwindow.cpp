70550d212b9539f151318f48396cbb3dfdbdd9cf
horsicq <horsicq@gmail.com>
2020-06-09 10:00:40 +0200

    Fix

--- gui_source/guimainwindow.cpp
+++ gui_source/guimainwindow.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2019 hors<horsicq@gmail.com>
+// Copyright (c) 2019-2020 hors<horsicq@gmail.com>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -35,7 +35,6 @@ GuiMainWindow::GuiMainWindow(QWidget *parent) :
 
     DialogOptions::loadOptions(&xOptions);
     adjust();
-
 
     if(QCoreApplication::arguments().count()>1)
     {
